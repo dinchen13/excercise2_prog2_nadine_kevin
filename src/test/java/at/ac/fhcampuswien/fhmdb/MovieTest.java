d13a0cb23488c0f04fb0c08394bdcea45bc46755
LAPTOP-4HEA5EIP\nadin <nadine.kirnbauer@stud.fh-campuswien.ac.at>
2023-03-12 22:07:58 +0100

    movie class clean

--- src/test/java/at/ac/fhcampuswien/fhmdb/MovieTest.java
+++ src/test/java/at/ac/fhcampuswien/fhmdb/MovieTest.java
@@ -1,20 +1,26 @@
 package at.ac.fhcampuswien.fhmdb;
-
-import static at.ac.fhcampuswien.fhmdb.Genre.*;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
-
-
 import java.util.List;
 
 public class MovieTest {
-
     @Test
-    void checks_if_movies_get_initialized(){
+    void initialized_movielist_contains_anything(){
         //When
         List<Movie> movieList = Movie.initializeMovies();
         //Then
         assertFalse(movieList.isEmpty());
     }
-
+    /*
+    @Test
+    void initialized_movielist_contains_a_specific_movie(){
+        //Given
+        String movie = "he";
+        //When
+        List<Movie> movieList = Movie.initializeMovies();
+        String movies =movieList.toString();
+        //Then
+        assertTrue(movies.contains(movie));
+    }
+    */
 }
